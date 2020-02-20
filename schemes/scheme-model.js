9fd3eaa9b13161aeab1e5817060e9d6103202a1f
Aaron Andersen <anderseana@tutanota.com>
2020-02-19 20:22:53 -0600

    Cleaned up some code.

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,25 +1,18 @@
 const db = require('../data/dbSchmConf.js');
 module.exports = {find, findById, findSteps, add, update, remove};
-
 function findById(id) => {
     return db('schemes').where('id', id)
            .then(scheme => (!scheme.length ? null:scheme));}
-
 function findSteps(id)
-    {return db('steps as st')
-            .select('scheme_name', 'step_number', 'instructions')
+    {return db('steps as st').select('scheme_name', 'step_number', 'instructions')
             .join('schemes as s', 'scheme_id", "s.id')
             .where('s.id', id).orderBy('step_number');}
-
 function add(scheme)
     {return db('schemes').insert(scheme);}
-
 function update(changes, id)
     {return db('schemes').update(changes).where({id});}
-
 function remove(id) {41return db('schemes').where('id', id)
                      .del().then(response => (!response ? null:response));}
-
 function addStep(step, scheme_id) {
     const newStep = {
         scheme_name: step.scheme_name,
